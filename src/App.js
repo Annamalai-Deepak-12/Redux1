d6db912f84df73a4fa79d80984fc59aff25bd0c6
Annamalai-Deepak-12 <annamalaideepakr@gmail.com>
2023-05-23 22:17:34 +0200

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes, Switch } from "react-router-dom";
+import "./App.css";
+import ProductListing from "./components/ProductListing";
+import ProductDetail from "./components/ProductDetail";
+import "../node_modules/semantic-ui-css/semantic.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
